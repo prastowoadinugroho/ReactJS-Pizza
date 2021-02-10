2528c3fe7803e204fc09a9fca57fd1fb9c6e2a4f
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-02-10 10:55:52 +0700

    Product element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Hero from './components/Hero/index';
+import Products from './components/Products';
+import { productData } from './components/Products/data';
 import { GlobalStyle } from './globalStyle';
 function App() {
   return (
     <Router>
       <GlobalStyle/>
       <Hero/>
+      <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/pizza-2.jpg';
+import product2 from '../../images/pizza-3.jpg';
+import product3 from '../../images/pizza-4.jpg';
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        price: 'Rp.10K',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Hawaiian Pizza',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        price: 'Rp.20K',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Veggiie Pizza',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        price: 'Rp.30K',
+        button: 'Add to Cart'
+    }
+
+]
